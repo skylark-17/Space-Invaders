66f7b34e10ccb480dac20e7402e6f38f65386709
Nikita Zhavoronkov <zhavoronkov.na@phystech.edu>
2022-10-18 16:47:28 +0300

    Removed unused includes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using static UnityEngine.Camera;
 using Random = UnityEngine.Random;
 
